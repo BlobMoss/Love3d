34bfeb6e23b6ea83b3599818d1a3cfcc543f3ae1
Melvin Bentinger <melvinbentinger2@gmail.com>
2021-02-22 20:23:53 +0100

    very bad camera movement

--- main.lua
+++ main.lua
@@ -1,24 +1,15 @@
 local matrix = require "matrix"
-
-depth = 4.0
+local camera = require "camera"
 
 cube = {
-    { {-0.5}, {-0.5}, {-0.5} 
-    },
-    { {0.5}, {-0.5}, {-0.5} 
-    },
-    { {-0.5}, {0.5}, {-0.5} 
-    },
-    { {0.5}, {0.5}, {-0.5} 
-    },
-    { {-0.5}, {-0.5}, {0.5} 
-    },
-    { {0.5}, {-0.5}, {0.5} 
-    },
-    { {-0.5}, {0.5}, {0.5} 
-    },
-    { {0.5}, {0.5}, {0.5} 
-    }
+    { {-0.5}, {-0.5}, {-0.5} },
+    { {0.5}, {-0.5}, {-0.5} },
+    { {-0.5}, {0.5}, {-0.5} },
+    { {0.5}, {0.5}, {-0.5} },
+    { {-0.5}, {-0.5}, {0.5} },
+    { {0.5}, {-0.5}, {0.5} },
+    { {-0.5}, {0.5}, {0.5} },
+    { {0.5}, {0.5}, {0.5} }
 }
 
 function love.load()
@@ -26,25 +17,19 @@ function love.load()
 end
 
 function love.update(dt)
-    angle = angle + dt
+    camera.update(dt)
 end
 
 function love.draw(dt)
     for i = 1, #cube, 1 
     do 
-        local point = cube[i]
+        local point = matrix.add(cube[i], camera.position) 
 
-        point = matrix.multiply(matrix.rotationX(angle), point)
-        point = matrix.multiply(matrix.rotationY(angle), point)
-        point = matrix.multiply(matrix.rotationZ(angle), point)
+        point = matrix.multiply(matrix.rotationX(camera.angleX), point)
+        point = matrix.multiply(matrix.rotationY(camera.angleY), point)
+        point = matrix.multiply(matrix.rotationZ(camera.angleZ), point)
 
-        local z = 1 / (depth - point[3][1])
-        local projection = {
-            {z, 0, 0},
-            {0, z, 0}
-        }
-
-        point = matrix.multiply(projection, point)
+        point = matrix.multiply(matrix.perspective(camera.depth, point), point)
         
         love.graphics.points(point[1][1] * 500 + 400, point[2][1] * 500 + 300)
     end
--- matrix.lua
+++ matrix.lua
@@ -1,15 +1,30 @@
 local matrix = {}
 
+function matrix.add( mtx1, mtx2 )
+	local mtx = {}
+
+	for i = 1, #mtx1 
+	do
+		local mtx3 = {}
+		mtx[i] = mtx3
+		for j = 1, #mtx1[1] 
+		do
+			mtx3[j] = mtx1[i][j] + mtx2[i][j]
+		end
+	end
+	return mtx
+end
+
 function matrix.multiply(mtx1, mtx2)
 	local mtx = {}
 
-	for i = 1, #mtx1
+	for i = 1, #mtx1, 1
     do
 		mtx[i] = {}
-		for j = 1, #mtx2[1] 
+		for j = 1, #mtx2[1], 1
         do
 			local newValue = mtx1[i][1] * mtx2[1][j]
-			for n = 2, #mtx1[1]
+			for n = 2, #mtx1[1], 1
             do
 				newValue = newValue + mtx1[i][n] * mtx2[n][j]
 			end
@@ -45,6 +60,15 @@ function matrix.rotationZ(angle)
 		{0, 0, 1}
 	}
 	return mtx
+end
+
+function matrix.perspective(depth, point)
+	local z = 1 / (depth - point[3][1])
+    local projection = {
+        {z, 0, 0},
+        {0, z, 0}
+    }
+	return projection
 end
 
 return matrix
